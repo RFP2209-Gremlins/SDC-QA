ac64aceb380c9100e4763a29e45d5ba53d4822b0
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2022-11-03 16:39:55 -0700

    /qa/questions get progress

--- server/routes.js
+++ server/routes.js
@@ -8,30 +8,43 @@ router.get('/', (req, res) => {
 router.get('/qa/questions', (req, res) => {
   const page = req.query.page || 1
   const count = req.query.count || 5
-  // ${req.query.product_id}
+
   const query =`
     SELECT json_build_object(
       'product_id', ${req.query.product_id},
       'results', (SELECT json_agg(
         json_build_object(
           'question_id', id,
-          'question_body', body
+          'question_body', body,
+          'question_date', to_timestamp(CAST(date_written as bigint)/1000),
+          'asker_name', asker_name,
+          'question_helpfulness', helpful,
+          'reported', ${false},
+          'answers', (SELECT json_agg(json_build_object(
+            'id', id,
+            'body', body,
+            'date', to_timestamp(CAST(date_written as bigint)/1000),
+            'answerer_name', answerer_name,
+            'helpfulness', helpful,
+            'photos', (SELECT json_agg(json_build_object(
+              'id', id,
+              'url', url
+            )) FROM photos WHERE answer_id = answers.id)
+          )) FROM answers WHERE question_id = questions.id AND reported = false)
         )) FROM questions WHERE product_id = ${req.query.product_id} AND reported = false)
     )`
 
   db.query(query)
-    .then(data => {res.status(200).send(data.rows)})
+    .then(data => {res.status(200).send(data.rows[0].json_build_object)})
     .catch(e => {console.log('get /qa/questions error', e); res.status(500).send(e)})
 })
 
 router.post('/qa/questions', (req, res) => {
   const query = 'INSERT INTO questions (product_id, body, date_written, asker_name, asker_email, reported, helpful) VALUES ($1, $2, $3, $4, $5, $6, $7)'
   db.query(query, [parseInt(req.body.product_id), req.body.body, new Date().getTime(), req.body.asker_name, req.body.asker_email, false, 0])
-    .then(data => {res.status(201).send({message: 'Created'})})
+    .then(data => {res.status(201).send({message: 'Created', date: new Date().getTime()})})
     .catch(e => {console.log('post /qa/questions error', e); res.status(500).send(e)})
 })
-
-
 
 module.exports = router;
 
